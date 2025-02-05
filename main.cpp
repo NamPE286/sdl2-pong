5331dc5de196ba37564e66d1c238dc10d84a5962
NamPE286 <nambuihung654@gmail.com>
2025-02-05 18:38:04 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 ﻿#include <iostream>
 #include "SDL2/SDL.h"
 
-int main(int argc, char* argv[]) {
+constexpr auto WIDTH = 640;
+constexpr auto HEIGHT = 480;
+constexpr auto FONT_SIZE = 32;
+
+struct procStat {
+	int frameCount, timerFPS, lastFrame, fps;
+};
+
+int SDL_main(int argc, char* argv[]) {
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
-	
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		std::cout << "Failed to init SDL" << std::endl;
+	}
+
+	if (SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer) < 0) {
+		std::cout << "Failed to create window and renderer" << std::endl;
+	}
 
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_RenderDrawPoint(renderer, 640 / 2, 480 / 2);
