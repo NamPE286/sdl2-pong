040aa6a81e2000cb3dffbb875b03cda3ca0f7642
NamPE286 <nambuihung654@gmail.com>
2025-02-05 21:18:13 +0700

    pong

--- main.cpp
+++ main.cpp
@@ -1,11 +1,67 @@
 ﻿#include <iostream>
 #include "SDL2/SDL.h"
 
-constexpr auto WIDTH = 640;
-constexpr auto HEIGHT = 480;
+constexpr auto WINDOW_WIDTH = 640;
+constexpr auto WINDOW_HEIGHT = 480;
+
+void center_window(SDL_Window* window) {
+	SDL_DisplayMode DM;
+
+	SDL_GetCurrentDisplayMode(0, &DM);
+	SDL_SetWindowPosition(window, (DM.w - WINDOW_WIDTH) / 2, (DM.h - WINDOW_HEIGHT) / 2);
+}
+
+void draw_net(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+	int cur = 0;
+
+	for (int y = 0; y < WINDOW_HEIGHT; y++) {
+		if (cur < 10) {
+			SDL_RenderDrawPoint(renderer, WINDOW_WIDTH / 2, y);
+		}
+
+		if (cur == 15) {
+			cur = 0;
+		}
+		else {
+			cur++;
+		}
+	}
+}
 
 int SDL_main(int argc, char* argv[]) {
-	SDL_Window* window = nullptr;
-	SDL_Renderer* renderer = nullptr;
+	SDL_Init(SDL_INIT_VIDEO);
 
+	SDL_Window* window = SDL_CreateWindow("Pong", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	bool running = true;
+
+	center_window(window);
+
+	while (running) {
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				running = false;
+			}
+			else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) {
+				running = false;
+			}
+		}
+
+		SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, 0xFF);
+		SDL_RenderClear(renderer);
+
+		draw_net(renderer);
+
+		SDL_RenderPresent(renderer);
+	}
+	
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
