f33524ea56e9fadd83d78d121dd2b5a93363c526
NamPE286 <nambuihung654@gmail.com>
2025-02-05 17:59:25 +0700

    draw a pixel

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 #include "SDL2/SDL.h"
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello world";
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderDrawPoint(renderer, 640 / 2, 480 / 2);
+
+	SDL_RenderPresent(renderer);
+	SDL_Delay(10000);
+
 	return 0;
 }
